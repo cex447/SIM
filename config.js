99925de442002ca9f12206e38b9349d19bf46dd7
cex447 <32179473+cex447@users.noreply.github.com>
2026-05-29 18:59:08 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,38 @@
+export const LINEAS = {
+
+    S1: {
+        letra: "D",
+        color: "#ff8a05",
+        origen: "PC",
+        destino: "NA"
+    },
+
+    S2: {
+        letra: "F",
+        color: "#39d40f",
+        origen: "PC",
+        destino: "PN"
+    },
+
+    L6: {
+        letra: "A",
+        color: "#7c4dbf",
+        origen: "PC",
+        destino: "SR"
+    },
+
+    L7: {
+        letra: "B",
+        color: "#805234",
+        origen: "PC",
+        destino: "TB"
+    },
+
+    L12: {
+        letra: "L",
+        color: "#b381db",
+        origen: "SR",
+        destino: "RE"
+    }
+
+};
